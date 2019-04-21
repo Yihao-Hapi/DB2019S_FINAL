6285fdd9473ff1e4e196c79af2c7c82be2116f28
iankdiaz <diazi@rpi.edu>
2019-04-20 20:13:38 -0400

    Implemented @konoha queries. Minor fixes

--- application.py
+++ application.py
@@ -7,17 +7,19 @@ def initialPrompt():
             "\t1. Specific procedure lookup\n"+\
             "\t2. Hospital complications lookup\n"+\
             "\t3. Find average statistics for an area\n"+\
+            "\t4. Find the safest hospital for given procedure\n"+\
+            "\t5. Find a provider for a procedure under a budget\n"+\
             "\tEXIT. Exit the program\n")
 
 #hospitalQuery not yet implemented
 if __name__ == "__main__":
     while(1):
-        functions = {"1": db.procedureQuery,"2": db.compQuery,"3":db.avgQuery}
+        functions = {"1": db.procedureQuery,"2": db.compQuery,"3":db.avgQuery,"4":db.Safest_Hospital,"5":db.Search_by_Budget}
         initialPrompt()
         command = db.safeInput()
         if (command == "EXIT" ):
             break
-        elif (command not in ["1","2","3"]):
+        elif (command not in ["1","2","3","4","5"]):
             print("ERROR: invalid input")
         else:
             functions[command]()
